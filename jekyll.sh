e285fa3bd0179e077a456ed2e2a63e42eb43baa7
David de Mingo <david@optersoft.com>
2022-10-23 07:26:49 +0000

    jekyll.sh now works fine

--- jekyll.sh
+++ jekyll.sh
@@ -2,45 +2,30 @@
 
 export JEKYLL_VERSION=3.8.6
 
-init() {
-
-    touch Gemfile.lock
-    chmod a+w Gemfile.lock
-
-    #rm .jekyll-metadata
-    touch .jekyll-metadata
-    chmod a+w .jekyll-metadata
-
-    docker volume create smx-8-jekyll
-    docker volume create smx-8-jekyll-bundle
-
-}
-
 update() {
-    
-    init
 
     docker run --rm \
+        --env JEKYLL_UID=$UID \
+        --env JEKYLL_GID=$UID \
         --volume="$PWD:/srv/jekyll" \
-        --volume="site:/srv/jekyll/_site" \
-        --volume="bundle:/usr/local/bundle" \
-        -it jekyll/jekyll:$JEKYLL_VERSION \
+        --volume="$PWD/_site:/srv/jekyll/_site" \
+        --volume="$PWD/_bundle:/usr/local/bundle" \
+        jekyll/jekyll:$JEKYLL_VERSION \
         bundle update
 }
 
 run() {
    
-    init
-
     docker run --rm \
-        --volume="$PWD:/srv/jekyll:Z" \
-        --volume="site:/srv/jekyll/_site:Z" \
-        --volume="bundle:/usr/local/bundle:Z" \
+        --env JEKYLL_UID=$UID \
+        --env JEKYLL_GID=$UID \
+        --volume="$PWD:/srv/jekyll" \
+        --volume="$PWD/_site:/srv/jekyll/_site" \
+        --volume="$PWD/_bundle:/usr/local/bundle" \
         --publish 4000:4000 \
         --publish 35729:35729 \
         jekyll/jekyll:$JEKYLL_VERSION \
         jekyll serve --livereload --incremental
-
 }
 
 case $1 in
